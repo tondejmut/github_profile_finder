b6d7a4778acf9b9d0d13215e16eb92ac6a778d37
Tonderai Mutanda <tonderaimutanda@gmail.com>
2018-05-29 15:02:05 +0200

    Profile finder, first commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value
+});
