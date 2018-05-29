e3750da08e7fc7808f4af02c7cb2efd2f1a972a2
Tonderai Mutanda <tonderaimutanda@gmail.com>
2018-05-29 15:32:48 +0200

    Added profile fetching functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
+// Innit Github
+const github = new Github;
+
 // Search Input
 const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
     // Get input text
-    const userText = e.target.value
+    const userText = e.target.value;
+    if(userText !== ''){
+        // Make http call
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message == 'Not Found') {
+                // Show alert
+
+            }else {
+                // Show profile
+            }
+        })
+    } else {
+        // Clear profile
+    }
 });
--- src/js/github.js
+++ src/js/github.js
@@ -1,1 +1,17 @@
- 
+class Github {
+    constructor(){
+    this.client_id = '53dc5d4160547478a699';
+    this.client_secret = 'aed908d49fbeb8360f7c4f2b4c3734bfdaf972c5';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}
+        ?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile
+        }
+    }
+}
